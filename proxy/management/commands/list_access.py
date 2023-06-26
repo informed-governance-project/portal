708a3dfdf924bb6cdbd9b7093a9196dc441ea028
Cédric Bonhomme <cedric@cedricbonhomme.org>
2023-06-26 09:13:47 +0200

    new: [commands] Added command to list access.

---
+++ proxy/management/commands/list_access.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+
+from proxy.models import ExternalToken
+
+
+class Command(BaseCommand):
+    help = """Revoke a user's access to a specified service.
+    Usage: ``python manage.py revoke_access --help``"""
+
+    def add_arguments(self, parser):
+        parser.add_argument("--username", type=str)
+        parser.add_argument("--module_name", type=str)
+
+    def handle(self, *args, **options):
+
+        accesses = ExternalToken.objects.all().order_by("user__username")
+
+        print("Username" + " " * 15 + "Module name")
+        print("-" * 34)
+        for access in accesses:
+            print("{} {} {}".format(access.user.username, " " * 15, access.module_name))
